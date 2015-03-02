8836387d35de0ba4752524853d196dc4f524a890
shunobaka <vesselin_minev@abv.bg>
2015-03-03 00:51:18 +0200

    Changed highscores file place

--- AFTH/Game/Game.cs
+++ AFTH/Game/Game.cs
@@ -11,7 +11,7 @@ namespace Game
 {
     class Game
     {
-        const string highScoresFile = "HighScores.txt";
+        const string highScoresFile = @"..\..\HighScores.txt";
         static int[] size = { 8, 8 }; // size[0] - width; size[1] - height
         static char[,] board = new char[size[0], size[1]]; //main game board
         static int x = size[0] / 2; // player x
